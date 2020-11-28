f32a65171ef9047491513b3d0704018188d0eeeb
nurun_vincent_lee <vincent.lee@nurun.com>
2020-11-28 14:35:59 -0500

    added cookie

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,12 +70,51 @@ $( "#pills-item3-tab" ).click(function() {
 });
 
 
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+function checkCookie() {
+	var username = getCookie("juliet");
+	if (username != "") {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 
 $(document).ready(function(){
 
 	var password_flag = false;
 
-	callPrompt()
+
+	// check cookie
+	if (!checkCookie()){
+		callPrompt();
+	} 
+
 
 	function callPrompt() {
 
@@ -84,6 +123,7 @@ $(document).ready(function(){
 			inputType: 'password',
 			callback: function (result) {
 				if(result=='juliet') {
+					setCookie('juliet', 1, 365);
 					return true;
 				}else {
 					callPrompt();
